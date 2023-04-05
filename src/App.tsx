1e60ae7fb3351b21ac8a79145dda3a213042cfaa
Tomasz Fidos <fidostomasz@gmail.com>
2023-04-05 18:06:40 +0200

    Complemented App and styled it.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+import { useState } from 'react';
+import axios from 'axios';
+
 import './App.css';
+import AppRoutes from './routes/AppRoutes';
+
+interface User {
+  isLogged: boolean,
+  jwt_token: string,
+}
 
 
 function App() {
+
+  const [user, setUser] = useState<User>({
+    isLogged: false,
+    jwt_token: '',
+  });
+  axios.defaults.headers.common['Authorization'] = 'Bearer ' + (user ? user.jwt_token : '');
+
   return (
-    <div className="App">
-      <p>Test</p>
+    <div className="app">
+      <AppRoutes user={user} />
     </div>
   );
 }
 
 export default App;
+export type { User };
